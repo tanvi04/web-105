f576c17df49b066065063cad9d053129ee8f5171
tanvi04 <45827267+tanvi04@users.noreply.github.com>
2021-01-21 17:57:23 +0530

    Add files via upload

---
+++ gauri/Historypage.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+public partial class Historypage : System.Web.UI.Page
+{
+    
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Bind();
+    }
+    public void Bind()
+    {
+        MySqlConnection con = new MySqlConnection("server=localhost;user id=Gauri;persistsecurityinfo=True;database=bank;password = Gauri");
+        con.Open();
+        MySqlCommand cmd = new MySqlCommand("select * from transaction", con);
+        MySqlDataAdapter adp = new MySqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        adp.Fill(ds);
+        GridView1.DataSource = ds;
+        GridView1.DataBind();
+        cmd.Dispose();
+        con.Close();
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("Homepage.aspx");
+    }
+}
